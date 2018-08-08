fd0b5b33e644fc4a5937c714951dba220092518a
ahsankhan1911 <ahsankhan1911@gmail.com>
2018-08-08 16:32:25 +0500

    some changes

--- server.js
+++ server.js
@@ -19,9 +19,7 @@ mongoose.connect('mongodb://nestmetric:nestmetric123@ds113692.mlab.com:13692/nes
     }
 });
 
-app.get('/', (req, res) => {
-  res.send("HELL WORLDDD HOW ARE YOU ALLL !!!")
-})
+
 
 app.use(cors())
 
@@ -31,6 +29,10 @@ app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 app.use('/api', require('./api'));
 
+app.get('/', (req, res) => {
+    res.send("HELL WORLDDD HOW ARE YOU ALLL !!!")
+  })
+  
 app.listen(port, function () {
     console.log('Running server on ' + port);
 });
